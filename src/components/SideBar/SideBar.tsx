163e9143364208f1b4eeded3501c56ff4e26a27d
vesmor <romsev.charles@gmail.com>
2024-11-08 02:04:48 -0500

    changed theme back to white text

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -9,34 +9,35 @@ import { MailIcon } from 'lucide-react';
 
 const SideBar: React.FC = () => {
     return (
-        <div className="sidebar p-4 h-full w-60 left-0 top-0 m-0 fixed overflow-x-hidden
-                        bg-foreground text-secondary-foreground  
+        <div className="sidebar z-3 p-4 h-full w-60 left-0 top-0 m-0 fixed overflow-x-hidden
+                        bg-foreground text-white  
                         flex flex-col justify-between">
             <div>
                 <div className='top-header mb-14'>
-                    
-                    <img src='assets/images/standin_profile64.png' alt="Profile" className="profile-pic w-40 h-32 rounded-3xl mx-auto mb-4 " />
+                    <a href='/'>
+                        <img src='assets/images/standin_profile64.png' alt="Profile" className="profile-pic w-40 h-32 rounded-3xl mx-auto mb-4 " />
+                    </a>
                     
                     <div className='name'>
                         <h1 className=" text-center text-xl font-bold">Sev Charles</h1>
                     </div>
                     
-                    <div className="blurb text-center mb-4">
+                    <div className="blurb text-center mb-4 text-gray-600">
                         <p>Computer Science student and Software Engineer creating solutions across software and hardware.</p>
                     </div>
                 </div>
                 <ul className="nav-links space-y-2 mb-4">
                     <li>
                         <Link to="/about" className='hover:border-transparent'>
-                            <SideBarButton>
-                                <PersonIcon className="mr-2 h-4 w-4" /> About
+                            <SideBarButton className='text-white'>
+                                <PersonIcon className="mr-2 h-4 w-4 text-white" /> About
                             </SideBarButton>
                         </Link>
                     </li>
                     <li>
                         <Link to="/projects" className='hover:border-transparent'>
-                            <SideBarButton>
-                                <BackpackIcon className="mr-2 h-4 w-4" /> Projects
+                            <SideBarButton className='text-white'>
+                                <BackpackIcon className="mr-2 h-4 w-4 text-white" /> Projects
                             </SideBarButton>
                         </Link>
                     </li>
@@ -46,19 +47,19 @@ const SideBar: React.FC = () => {
                 {/* <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-600">GitHub</a> */}
                 <SideBarButton className='flex items-center justify-center hover:bg-foreground hover:border-none '>
                 <a href="https://github.com/vesmor" target="_blank" rel="noopener noreferrer" className='text-inherit'>
-                    <GitHubLogoIcon className="w-6 h-6 hover:text-gray-600" />
+                    <GitHubLogoIcon className="w-6 h-6 hover:text-gray-600 text-white" />
                 </a>
                 </SideBarButton>
                 {/* <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-600">LinkedIn</a> */}
                 <SideBarButton className='flex items-center justify-center hover:bg-foreground hover:border-none '>
                     <a href="https://www.linkedin.com/in/sev-charles/" target="_blank" rel="noopener noreferrer" className='text-inherit'>
-                        <LinkedInLogoIcon className="w-6 h-6 hover:text-gray-600" />
+                        <LinkedInLogoIcon className="w-6 h-6 hover:text-gray-600 text-white" />
                     </a>
                 </SideBarButton>
                 
                 <SideBarButton className='flex items-center justify-center hover:bg-foreground hover:border-none '>
                     <a href="mailto:romsev.charles@gmail.com" className='text-inherit'>
-                        <MailIcon className="w-6 h-6 hover:text-gray-600" />
+                        <MailIcon className="w-6 h-6 hover:text-gray-600 text-white" />
                     </a>
                 </SideBarButton>
             </div>
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,62 +1,62 @@
 import AnimatedPage from "@/animation/AnimatedPage";
 import ProjectCard from "@/components/ProjectCards/ProjectCard";
 import SideBar from "@/components/SideBar/SideBar";
-
+import './Projects.css';
 
 function Projects(){
     return (
         <div>
 
             <SideBar />
-            <div className="content ml-60 bg-background text-white w-[calc(100vw-240px)] h-screen overflow-x-hidden overflow-y-scroll no-scrollbar">
-            <AnimatedPage>
-                <h1 className="ml-16">Projects</h1>
-                <div className="projectcard-container ml-16 m-16 py-4">
-                    <ul>
-                        <ProjectCard 
-                            name="Innovative App"
-                            description="An app that revolutionizes the way we interact with technology."
-                            image="assets/images/cat_standin.jpg"
-                            link="innovative-app"
-                        />
-                        <ProjectCard 
-                            name="Eco-Friendly Gadget"
-                            description="A gadget designed to reduce environmental impact."
-                            image="assets/images/coquette_mouse.jpg"
-                            link="eco-friendly-gadget"
-                        />
-                        
-                        <ProjectCard 
-                            name="Smart Home Device"
-                            description="A device that makes your home smarter and more efficient."
-                            image="assets/images/briefcase_standin.png"
-                            link="smart-home-device"
-                        />
-                        
-                        <ProjectCard 
-                            name="Health Tracker"
-                            description="A wearable device that monitors your health metrics."
-                            image="assets/images/coquette_mouse.jpg"
-                            link="health-tracker"
-                        />
-                        
-                        <ProjectCard 
-                            name="Educational Platform"
-                            description="An online platform that enhances learning experiences."
-                            image="assets/images/coquette_mouse.jpg"
-                            link="educational-platform"
-                        />
-                        
-                        <ProjectCard 
-                            name="Blah Blah Blah"
-                            description="Ipsum dolor sit amet, consectetur adipiscing elit."
-                            image="assets/images/briefcase_standin.png"
-                            link="blah-blah-blah"
-                        />
-                    </ul>
+            <div className="content ml-60 text-white w-[calc(100vw-240px)] h-screen overflow-x-hidden overflow-y-scroll no-scrollbar">
+                <AnimatedPage>
+                    <h1 className="ml-16">Projects</h1>
+                    <div className="projectcard-container ml-16 m-16 py-4">
+                        <ul>
+                            <ProjectCard 
+                                name="Innovative App"
+                                description="An app that revolutionizes the way we interact with technology."
+                                image="assets/images/cat_standin.jpg"
+                                link="innovative-app"
+                            />
+                            <ProjectCard 
+                                name="Eco-Friendly Gadget"
+                                description="A gadget designed to reduce environmental impact."
+                                image="assets/images/coquette_mouse.jpg"
+                                link="eco-friendly-gadget"
+                            />
+                            
+                            <ProjectCard 
+                                name="Smart Home Device"
+                                description="A device that makes your home smarter and more efficient."
+                                image="assets/images/briefcase_standin.png"
+                                link="smart-home-device"
+                            />
+                            
+                            <ProjectCard 
+                                name="Health Tracker"
+                                description="A wearable device that monitors your health metrics."
+                                image="assets/images/coquette_mouse.jpg"
+                                link="health-tracker"
+                            />
+                            
+                            <ProjectCard 
+                                name="Educational Platform"
+                                description="An online platform that enhances learning experiences."
+                                image="assets/images/coquette_mouse.jpg"
+                                link="educational-platform"
+                            />
+                            
+                            <ProjectCard 
+                                name="Blah Blah Blah"
+                                description="Ipsum dolor sit amet, consectetur adipiscing elit."
+                                image="assets/images/briefcase_standin.png"
+                                link="blah-blah-blah"
+                            />
+                        </ul>
 
-                </div>
-            </AnimatedPage>
+                    </div>
+                </AnimatedPage>
             </div>
         </div>
     )
