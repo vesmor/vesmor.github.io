0cf892b5b3434a4c4b672e0cf299aa1302f9e489
vesmor <romsev.charles@gmail.com>
2024-11-08 02:05:17 -0500

    added twinkling night background, started writing in aboutme

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -6,32 +6,40 @@ export default function About() {
         <div className="text-white ">
             <SideBar />
             
-            <div className="container ml-60 p-4 w-[calc(100vw-240px)]">
+            <div className="container ml-60 p-4 w-[calc(100vw-240px)] overflow-x-hidden overflow-y-scroll no-scrollbar">
             <AnimatedPage>
                 <h1 className="text-4xl font-bold mb-6">About Me</h1>
-                <section className="mb-6">
-                    <h2 className="text-2xl font-semibold mb-2">Inspirations</h2>
-                    <p className="text-lg">
-                        I draw inspiration from the world around me, including nature, technology, and the people I meet. 
-                        My family and friends have always been a source of motivation and support.
-                    </p>
-                </section>
-                <section className="mb-6">
-                    <h2 className="text-2xl font-semibold mb-2">Aspirations</h2>
-                    <p className="text-lg">
-                        My aspirations include becoming a leader in my field, continuously learning and growing, and 
-                        making a positive impact on the community. I aim to innovate and contribute to projects that 
-                        can change lives for the better.
-                    </p>
-                </section>
-                <section>
-                    <h2 className="text-2xl font-semibold mb-2">Future Plans</h2>
-                    <p className="text-lg">
-                        In the future, I plan to further my education, take on challenging projects, and collaborate with 
-                        like-minded individuals. I hope to travel, explore new cultures, and gain diverse experiences 
-                        that will enrich my personal and professional life.
-                    </p>
-                </section>
+                <div className="aboutme-container ml-16 py-16 h-screen">
+                    <section className="mb-6">
+                        <h2 className="text-2xl font-semibold mb-2">Inspirations</h2>
+                        <p className="text-lg">
+                            I am a student closing in on my Bachelor's degree in Computer Science at the University of Central Florida. I currently have the most experience in full-stack Web Development but have expanded my knowledge to include working in other areas such as embedded systems and working with simulation software. Currently, I am working on a project with the Florida Space Institute to create a teleoperation console for their RERASSOR rover program.
+                            <br />
+                        </p>
+                    </section>
+
+                    <section className="mb-6">
+                        <h2 className="text-2xl font-semibold mb-2">Aspirations</h2>
+                        <p className="text-lg">
+                            Although I'm finishing up school, I consider myself to be a lifelong student. I'm always looking for new things to learn and new ways to grow.
+                            and I'm excited yet nervous to see where my journey takes me next. 
+                            <br />
+                            My aspirations include becoming a leader in my field, continuously learning and growing, and 
+                            making a positive impact on the community. I aim to innovate and contribute to projects that 
+                            can change lives for the better.
+                        </p>
+                    </section>
+                 
+                    <section>
+                        <h2 className="text-2xl font-semibold mb-2">Future Plans</h2>
+                        <p className="text-lg">
+                            In the future, I plan to further my education, take on challenging projects, and collaborate with 
+                            like-minded individuals. I hope to travel, explore new cultures, and gain diverse experiences 
+                            that will enrich my personal and professional life.
+                        </p>
+                    </section>
+                    
+                </div>
             </AnimatedPage>
             </div>
         </div>
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -8,34 +8,37 @@ import {ExitAnimation , ExitButton} from '@/animation/ExitLanding';
 function LandingPage() {
   return (
     <ExitAnimation>
-      
-      <div className="landingpage-div ">
-        <div className="introduction-plate text-secondary-foreground">
-          <div className='header-plate'>
-            <div className='name-plate'>
-              <h1>Hi, I'm Sev.</h1>
+      <div className='LandingPageContainer' >
+        
+        <div className="stars"></div>
+        <div className="twinkling"></div>
+        <div className="landingpage-div">
+          <div className="introduction-plate text-secondary-foreground">
+            <div className='header-plate'>
+              <div className='name-plate'>
+                <h1>Hi, I'm Sev.</h1>
+              </div>
+              <div className="subheading-plate">
+                <h2>Welcome to my page!</h2>
+              </div>
             </div>
-            <div className="subheading-plate">
-              <h2>Welcome to my page!</h2> 
-            </div>      
+            <div className="graphics-plate">
+        
+              <img src={'assets/images/standin_profile.jpg'} alt="graphic"/>
+        
+            </div>
           </div>
-          <div className="graphics-plate">
-            
-            <img src={'assets/images/standin_profile.jpg'} alt="graphic"/>
-            
+          <ExitButton>
+          <div className="buttons-plate">
+            <div className='aboutme' id='about-btn'>
+              <ShinyButtonLink text='Learn more about me!' link='/about'></ShinyButtonLink>
+            </div>
+            <div className='aboutme' id='project-btn'>
+              <ShinyButtonLink text="See what I've been up to!" link='/projects'></ShinyButtonLink>
+            </div>
           </div>
+          </ExitButton >
         </div>
-
-        <ExitButton>
-        <div className="buttons-plate">
-          <div className='aboutme' id='about-btn'>
-            <ShinyButtonLink text='Learn more about me!' link='/about'></ShinyButtonLink>
-          </div>
-          <div className='aboutme' id='project-btn'>
-            <ShinyButtonLink text="See what I've been up to!" link='/projects'></ShinyButtonLink>
-          </div>
-        </div>
-        </ExitButton >
 
       </div>
 
