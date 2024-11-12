5f91690e1c0c83e16a25cd32348ff683d76a4f1b
vesmor <romsev.charles@gmail.com>
2024-11-12 04:58:06 -0500

    hopefully fix routing issues

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename='/'>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 )
